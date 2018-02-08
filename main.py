12d964eb8214c92f7288309b4b73030fd0ab1651
tiabas <kevin@urbandoor.co>
2018-02-07 21:05:40 -0800

    Add CORS support. Fix docker-compose

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request, jsonify, abort
 from flask_restful import Resource, Api
 from google.appengine.api import app_identity
-from google.appengine.ext import ndb
+from flask_cors import CORS
+
+import models
 
 app = Flask(__name__)
-app.config['DEBUG'] = False
+app.config['DEBUG'] = True
+
+# Add support for Restful api
 api = Api(app)
 
-
-class OfferModel(ndb.Model):
-    creatorAddress = ndb.StringProperty()
+# Add CORS support for all domains
+CORS(app, origins="*", allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+    supports_credentials=True)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -24,14 +29,14 @@ class Index(Resource):
 class Offers(Resource):
     def get(self):
         """ Return a list of existing loan offers"""
-        offers = OfferModel.query().fetch()
-        offers_list = [{'id': offer.creatorAddress } for offer in offers]
+        offers = models.OfferModel.query().fetch()
+        offers_list = [offer.to_dict() for offer in offers]
         return jsonify(offers=offers_list)
 
     def post(self):
         if not request.json:
             abort(400, {"error": "Expected application/json"})
-        offer = OfferModel(creatorAddress="0x123456")
+        offer = models.OfferModel(**request.json)
         key = offer.put()
         return { 'id': key.id() }, 201
 
