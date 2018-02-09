79abc9ad813170857476325a580f177efbf96f4f
tiabas <kevin@urbandoor.co>
2018-02-08 22:37:20 -0800

    update data model

--- models.py
+++ models.py
@@ -3,8 +3,11 @@ from google.appengine.ext import ndb
 class OfferModel(ndb.Model):
     lenderAddress = ndb.StringProperty(required=True)
     tokenPair = ndb.StringProperty(required=True)
+    quoteTokenAddress = ndb.StringProperty(required=True)
+    baseTokenAddress = ndb.StringProperty(required=True)
     loanQuantity = ndb.IntegerProperty(required=True)
     loanToken = ndb.StringProperty(required=True)
+    loanTokenAddress = ndb.StringProperty(required=True)
     costAmount = ndb.IntegerProperty(required=True)
     costToken = ndb.StringProperty(required=True)
     ecSignature = ndb.StringProperty(required=True)
