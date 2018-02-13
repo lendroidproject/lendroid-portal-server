b5024d7183ec00254c33d4d23647bfff695b8fae
tiabas <kevin@urbandoor.co>
2018-02-12 20:57:36 -0800

    Add token symbols to models

--- models.py
+++ models.py
@@ -4,9 +4,12 @@ class OfferModel(ndb.Model):
     lenderAddress = ndb.StringProperty(required=True) # loan creator wallet adddress
     market = ndb.StringProperty(required=True) # token pair
     loanTokenAddress = ndb.StringProperty(required=True) # OMG contract address
+    loanTokenSymbol = ndb.StringProperty(required=True) # OMG contract address
     loanTokenAmount = ndb.StringProperty(required=True) # 1000
+    loanCostTokenSymbol = ndb.StringProperty(required=True) # 1000
     loanCostTokenAddress = ndb.StringProperty(required=True) # ETH contract address
     loanCostTokenAmount = ndb.StringProperty(required=True) # 100
+    loanInterestTokenSymbol = ndb.StringProperty(required=True) # 0.01
     loanInterestTokenAmount = ndb.StringProperty(required=True) # 0.01
     loanInterestTokenAddress = ndb.StringProperty(required=True) # ETH contract address
     ecSignature = ndb.StringProperty(required=True)
